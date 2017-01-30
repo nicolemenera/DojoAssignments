f3d6831261a07fcb1059b927e351abd5d1c3f99d
nicolemenera <nicolemenera@gmail.com>
2017-01-30 14:25:01 -0800

    idodelare assignments added to javascript folder

---
+++ WebFundamentals/Javascript/IDoDeclareAssignment/idodeclare.js
@@ -0,0 +1,41 @@
+<script type="text/javascript">
+function todaydate(){
+    var today_date= new Date()
+    var myyear=today_date.getYear()
+    var mymonth=today_date.getMonth()+1
+    var mytoday=today_date.getDate()
+    document.write(myyear+"/"+mymonth+"/"+mytoday)
+}
+</script>
+var age=92;
+console.log("you look " + age + " years old!");
+
+var friends=7.5;
+console.log("I have " + friends + " !");
+
+var measurment=-1149;
+console.log("Dig " + measurment + " kilometers");
+
+var numPups=34;
+console.log("I have " + numPups + " puppies!");
+
+
+var shortString='This is a short string';
+console.log('shortString');
+
+var longString='This is a very very very very very very very very very very very very long string';
+console.log('longString');
+
+var adorbsString='puppies kittens pandas piggies';
+console.log('adorbsString');
+
+
+var icecreamIsTasty=true;
+console.log("Is icecream is the best? " + icecreamIsTasty);
+
+var tierd=false;
+console.log("Are you awake? " + tierd);
+
+
+var x;
+console.log(x);
