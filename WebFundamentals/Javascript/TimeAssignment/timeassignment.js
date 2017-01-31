32bf098b9b6fa66c45c452b1f7492f36ff323bae
nicolemenera <nicolemenera@gmail.com>
2017-01-31 14:12:04 -0800

    added completed TimeAssignment and the starting of BirthdayCountdown to Javascript folder

---
+++ WebFundamentals/Javascript/TimeAssignment/timeassignment.js
@@ -0,0 +1,11 @@
+// function timeAssignment (HOUR, MINUTE, PERIOD) {
+//   var num = 8;
+//   var 
+// }
+
+
+var HOUR = 8;
+var MINUTE = 50;
+var PERIOD = "AM";
+
+console.log("It's currently: " + HOUR + ':' + MINUTE + ' ' + PERIOD);
