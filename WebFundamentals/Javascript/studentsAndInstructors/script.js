63d853f0c69ca33ee951fcbf9e749cb680ea5078
nicolemenera <nicolemenera@gmail.com>
2017-02-01 15:11:02 -0800

    added studentsAndInstructors assignment to Javascript folder

---
+++ WebFundamentals/Javascript/studentsAndInstructors/script.js
@@ -0,0 +1,15 @@
+var students = [ 
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+]
+
+function nameOutput (arr) {
+  for (var idx = 0; idx < arr.length; idx++ ) {
+    console.log( arr[idx].first_name + " " + arr[idx].last_name );
+
+  }
+}
+
+nameOutput(students);
