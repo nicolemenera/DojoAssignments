a6ea218ff48fab82c5ba8cbe286daa5c437b0e6f
Nikki Menera <nikki@Jamess-MacBook-Pro.local>
2017-02-12 22:29:18 -0800

    completed flask and mysql assignment

---
+++ Python/flaskMySQL/communication/server.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from mysqlconnection import MySQLConnector
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+app = Flask(__name__)
+mysql = MySQLConnector(app,'emails')
+app.secret_key = "llamallamallamamoo"
+@app.route('/', methods=['GET'])
+def index():
+  return render_template("index.html")
+  
+  
+  
+#@app.route('/process', methods=['POST'])
+#def submit():
+#  if len(request.form['email']) < 1:
+#   flash("Email cannot be blank!")
+# elif not EMAIL_REGEX.match(request.form['email']):
+#   flash("Invalid Email Address!")
+# else:
+#   flash("Success!")
+# return redirect('/')
+    
+    
+@app.route('/emails', methods=['POST', 'GET'])
+def handle_request():
+  if request.method == 'POST':
+    if len(request.form['email']) < 1:
+      flash("Email cannot be blank!")
+    elif not EMAIL_REGEX.match(request.form['email']):
+      flash("Invalid Email Address!")
+    else:
+      query = "INSERT INTO users (email, create_time, update_time) VALUES (:email, NOW(), NOW())"
+      data = {
+        'email': request.form['email'], 
+      }
+      mysql.query_db(query, data)
+      flash("Success!")
+    return redirect('/emails')
+  elif request.method == 'GET':
+    query = 'SELECT * FROM users'
+    users = mysql.query_db(query)
+    return render_template("success.html", users=users)
+    
+@app.route('/delete', methods=['POST'])
+def delete():
+  query = "DELETE FROM users WHERE email ='" + request.form['email'] + "'"
+  data = {'email': request.form['email']}
+  print query
+  mysql.query_db(query)
+  return redirect('/emails')
+    
+app.run(debug=True)
+  
--- Python/myEnvironments/Dojo_Survey/server.py
+++ Python/myEnvironments/Dojo_Survey/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 app = Flask(__name__)
 @app.route('/')
 
@@ -7,12 +7,11 @@ def survey():
 
 @app.route('/result', methods=['POST'])
 def create_user():
-   print "Got Post Info"
-   # we'll talk about the following two lines after we learn a little more
-   # about forms
-   # name = request.form['name']
-   # Dojo_location = request.form['location']
-   # language = request.form['language']
-   # redirects back to the '/' route
+    if len(request.form['name']) < 1:
+        flash("Name cannot be empty!") # just pass a string to the flash function
+    else:
+        flash("Success! Your name is {}".format(request.form['name'])) # just pass a string to the flash function
+    return redirect('/') 
+
    return render_template("result.html")
 app.run(debug=True) # run our server
